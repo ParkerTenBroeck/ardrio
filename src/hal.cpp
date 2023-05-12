62f944070e3045d1238deae004ba8b811ad3b808
Parker TenBroeck <51721964+ParkerTenBroeck@users.noreply.github.com>
2023-05-11 21:26:40 -0400

    added digital output and pwm output to hal

--- src/hal.cpp
+++ src/hal.cpp
@@ -1,7 +1,6 @@
 #include "hal.h"
 
 #include <atomic>
-
 #include "esp32-hal-ledc.h"
 
 //------------------ pin handle
@@ -10,11 +9,11 @@ uint8_t PinHandle::physicalPinNumber() { return this->pin; }
 
 PinHandle::PinHandle() { this->pin = 255; }
 
-PinHandle::PinHandle(uint8_t pin, PinHandleCreation* ctx) { this->pin = pin; }
+PinHandle::PinHandle(uint8_t pin, HandleCreation* ctx) { this->pin = pin; }
 
 //------------------ pin handle
 
-PinHandleCreation* create_pin_handle_creation() { return NULL; }
+HandleCreation* create_pin_handle_creation() { return NULL; }
 
 enum PinSettingVarient {
   StrappingPin,
@@ -58,11 +57,42 @@ enum PinSettingVarient {
 
 struct DOutputSetting {
   bool current_val : 1;
-  bool disable_val : 1;
-  bool should_stop_output : 1;
+  bool safety_critical_val : 1;
+  bool high_power_val : 1;
+  // whether or not to switch to disable_val when output is disabled
+  bool safety_critical : 1;
+  // whether or not to switch to disable_val when a brown out occurs
+  bool high_power_consumption : 1;
 };
 
-struct PwmOutputSetting {};
+struct PwnTimerSetting {
+  uint8_t resolution;
+  bool exists : 1;
+  bool safety_critical : 1;
+  bool high_power_consuption : 1;
+  uint32_t frequency;
+  uint32_t phase;
+  uint32_t disabled_frequency;
+  uint32_t disabled_phase;
+  uint32_t brownout_frequency;
+  uint32_t brownout_phase;
+};
+
+struct PwmOutputSetting {
+  uint8_t channel : 4;  // 16 channels max
+
+  // whether or not to switch to disable_val when output is disabled
+  bool safety_critical : 1;
+  // if this device is safety critical and saftey critical devices are disabled
+  // this is the value the pin will be set to
+  bool safety_critical_disabled_val : 1;
+
+  // whether or not to switch to disable_val when a brown out occurs
+  bool high_power_consumption : 1;
+  // if this device is high power and high power devices are disable
+  // what is the value of the pin()
+  bool high_power_disabled_val : 1;
+};
 
 struct DacOutputSetting {
   uint16_t val;
@@ -105,7 +135,33 @@ struct PinAbilities {
 };
 
 #define NUM_PINS 40
+#ifdef SOC_LEDC_SUPPORT_HS_MODE
+#define NUM_PWM_CHANNELS           (SOC_LEDC_CHANNEL_NUM<<1)
+#else
+#ifndef SOC_LEDC_CHANNEL_NUM
+#define SOC_LEDC_CHANNEL_NUM 16
+#endif
+#define NUM_PWM_CHANNELS           (SOC_LEDC_CHANNEL_NUM)
+#endif
+
+PwnTimerSetting pwm_config[NUM_PWM_CHANNELS] = {};
+
 std::atomic<bool> pin_settings_lock(false);
+
+void lock_pin_settings_critical() {
+  while (pin_settings_lock.exchange(true, std::memory_order_acquire)) {
+  }
+  noInterrupts();
+}
+
+void unlock_pin_settings_critical() {
+  pin_settings_lock.store(false, std::memory_order_release);
+  interrupts();
+}
+
+
+std::atomic<bool> disable_high_power(false);
+std::atomic<bool> disable_safety_critical(false);
 PinSetting pin_settings[NUM_PINS] = {};
 
 const PinAbilities pin_abilities[NUM_PINS] = {
@@ -177,40 +233,102 @@ const PinAbilities pin_abilities[NUM_PINS] = {
 };
 
 void run_hal_initialization() {
+  lock_pin_settings_critical();
   for (int i = 0; i < NUM_PINS; i++) {
     pin_settings[i].varient = None;
   }
+  unlock_pin_settings_critical();
 }
 
-void hal_disable_output() {}
+void hal_disable_output() {
+  lock_pin_settings_critical();
+  disable_safety_critical.store(true, std::memory_order_relaxed);
+  for(int i = 0; i < NUM_PINS; i ++){
+    switch(pin_settings[i].varient){
+      case DOutput:{
+        if(pin_settings[i].varients.DOutput.safety_critical){
+          digitalWrite(i, pin_settings[i].varients.DOutput.safety_critical_val);
+        }
+      }
+        break;
+      case PwmOutput:{
+        if(pin_settings[i].varients.PwmOutput.safety_critical){
+          ledcDetachPin(i);
+          digitalWrite(i, pin_settings[i].varients.PwmOutput.safety_critical_disabled_val);
+        }
+      }
+        break;
 
-void hal_enable_output() {}
+      default:
+        break;
+    }
+  }
+  unlock_pin_settings_critical();
+}
 
-void hal_force_disable() {}
+void hal_enable_output() {
+  lock_pin_settings_critical();
+  disable_safety_critical.store(false, std::memory_order_relaxed);
+  for(int i = 0; i < NUM_PINS; i ++){
+    switch(pin_settings[i].varient){
+      case DOutput:{
+        if(pin_settings[i].varients.DOutput.safety_critical){
+          digitalWrite(i, pin_settings[i].varients.DOutput.current_val);
+        }
+      }
+        break;
+      case PwmOutput:{
+        if(pin_settings[i].varients.PwmOutput.safety_critical){
+          ledcAttachPin(i, pin_settings[i].varients.PwmOutput.channel);
+        }
+      }
+        break;
 
-void hal_brownout_output_start() {}
+      default:
+        break;
+    }
+  }
+  unlock_pin_settings_critical();
+}
 
-void hal_brownout_output_stop() {}
+void hal_force_disable() {
+  hal_disable_output();
+}
+
+void hal_brownout_output_start() {
+  lock_pin_settings_critical();
+  disable_high_power.store(true, std::memory_order_relaxed);
+  unlock_pin_settings_critical();
+}
+
+void hal_brownout_output_stop() {
+  lock_pin_settings_critical();
+  disable_high_power.store(false, std::memory_order_relaxed);
+  unlock_pin_settings_critical();
+}
+
 
 PinHandle create_pin_handle(uint8_t pin, PinSettingVarient setting,
-                            PinSettingVarients data = {}) {
-  while (pin_settings_lock.exchange(true, std::memory_order_acquire)) {
-  }
+                            PinSettingVarients data = {}, bool lock = true) {
+  if(lock)
+    lock_pin_settings_critical();
   assert(pin < NUM_PINS);
   assert(pin_settings[pin].varient == None);
   assert(pin_abilities[pin].abilities[setting]);
   pin_settings[pin].varient = setting;
   pin_settings[pin].varients = data;
-  pin_settings_lock.store(false, std::memory_order_release);
+  if(lock)
+    unlock_pin_settings_critical();
   return PinHandle(pin, create_pin_handle_creation());
 }
 
-void delete_pin_handle(PinHandle handle) {
-  while (pin_settings_lock.exchange(true, std::memory_order_acquire)) {
-  }
+void delete_pin_handle(PinHandle handle, bool lock = true) {
+  if(lock)
+    lock_pin_settings_critical();
   assert(pin_settings[handle.physicalPinNumber()].varient != None);
   pin_settings[handle.physicalPinNumber()].varient = None;
-  pin_settings_lock.store(false, std::memory_order_release);
+  if(lock)
+    unlock_pin_settings_critical();
 }
 
 //---------------- analog read pin
@@ -299,16 +417,21 @@ bool inturrupt_pin_read(PinHandle& handle);
 void delete_inturrupt_pin(PinHandle handle);
 
 //---------------- digital output pin
-PinHandle create_digital_output(uint8_t pin, bool disabled_value,
-                                bool should_disable_output = true) {
+PinHandle create_digital_output(uint8_t pin, bool disabled_value, bool brownout_val,
+                                bool should_disable_output, bool should_brown_out) {
+  lock_pin_settings_critical();
   auto handle = create_pin_handle(pin, DOutput,
                                   PinSettingVarients{.DOutput{
                                       .current_val = disabled_value,
-                                      .disable_val = disabled_value,
-                                      .should_stop_output = disabled_value,
-                                  }});
+                                      .safety_critical_val = disabled_value,
+                                      .high_power_val = brownout_val,
+                                      .safety_critical = should_disable_output,
+                                      .high_power_consumption = should_brown_out,
+                                  }}, false);
+
   pinMode(pin, OUTPUT);
   digitalWrite(handle.physicalPinNumber(), disabled_value);
+  unlock_pin_settings_critical();
   return handle;
 }
 
@@ -318,13 +441,102 @@ bool digital_output_read(PinHandle& handle) {
 }
 
 void digital_output_write(PinHandle& handle, bool value) {
+  lock_pin_settings_critical();
   assert(pin_settings[handle.physicalPinNumber()].varient == DOutput);
-  digitalWrite(handle.physicalPinNumber(), value);
+  auto setting = &pin_settings[handle.physicalPinNumber()].varients.DOutput;
+  setting->current_val = value;
+  if (!((disable_high_power.load(std::memory_order_relaxed) &&
+         setting->high_power_consumption) ||
+        (disable_safety_critical.load(std::memory_order_relaxed) &&
+         setting->safety_critical))) {
+    digitalWrite(handle.physicalPinNumber(), value);
+  }
+  unlock_pin_settings_critical();
 }
 
 void delete_digital_output(PinHandle handle) {
+  lock_pin_settings_critical();
   assert(pin_settings[handle.physicalPinNumber()].varient == DOutput);
   digitalWrite(handle.physicalPinNumber(), false);
   pinMode(handle.physicalPinNumber(), INPUT);
-  delete_pin_handle(handle);
+  delete_pin_handle(handle, false);
+  unlock_pin_settings_critical();
+}
+
+
+//---------------- pwm generators
+
+uint8_t PwmHandle::channel(){
+  return this->_channel;
+}
+
+PwmHandle::PwmHandle(uint8_t channel, HandleCreation* ctx){
+  this->_channel = channel;
+}
+
+PwmHandle create_pwm_generator(uint8_t resolution, uint32_t frequecy, uint32_t phase){
+  for(uint8_t i = 0; i < NUM_PWM_CHANNELS; i ++){
+    if(!pwm_config[i].exists){
+      assert(ledcSetup(i, frequecy, resolution));
+      ledcWrite(i, phase);
+      return PwmHandle(i, create_pin_handle_creation());
+    }
+  }
+  assert(false);
+}
+void update_pwm_duty(PwmHandle& handle, uint32_t phase){
+  lock_pin_settings_critical();
+  assert(pwm_config[handle.channel()].exists);
+  ledcWrite(handle.channel(), phase);
+  unlock_pin_settings_critical();
+}
+void update_pwm_frequency(PwmHandle& handle, uint32_t frequency){
+  lock_pin_settings_critical();
+  assert(pwm_config[handle.channel()].exists);
+  ledcWriteTone(handle.channel(), frequency);
+  unlock_pin_settings_critical();
+}
+void update_pwm_resolution(PwmHandle& handle, uint8_t resolution){
+  lock_pin_settings_critical();
+  assert(pwm_config[handle.channel()].exists);
+  ledcChangeFrequency(handle.channel(), ledcReadFreq(handle.channel()), resolution);
+  unlock_pin_settings_critical();
+}
+
+void delete_pwm_generator(PwmHandle handle){
+  lock_pin_settings_critical();
+  assert(pwm_config[handle.channel()].exists);
+  ledcWrite(handle.channel(), 0);
+  pwm_config[handle.channel()].exists = false;
+  //TODO. possibly use ledc_stop()
+  unlock_pin_settings_critical();
+}
+
+//---------------- pwm output
+
+PinHandle create_pwn_output(uint8_t pin, PwmHandle& pwm_handle, bool disabled_value, bool brownout_val,
+                                bool should_disable_output , bool should_brown_out){
+  lock_pin_settings_critical();
+
+  auto handle = create_pin_handle(pin, PwmOutput,
+                                  PinSettingVarients{.PwmOutput{
+                                    .channel = pwm_handle.channel(),
+                                    .safety_critical = should_disable_output,
+                                    .safety_critical_disabled_val = disabled_value,
+                                    .high_power_consumption = should_brown_out,
+                                    .high_power_disabled_val = brownout_val
+                                  }}, false);
+
+  pinMode(pin, OUTPUT);
+  ledcAttachPin(pin, pwm_handle.channel());
+  unlock_pin_settings_critical();
+  return handle;
+}
+
+void delete_pwm_output(PinHandle handle){
+  lock_pin_settings_critical();
+  ledcDetachPin(handle.physicalPinNumber());
+  pinMode(handle.physicalPinNumber(), INPUT);
+  delete_pin_handle(handle, false);
+  unlock_pin_settings_critical();
 }
--- src/hal.h
+++ src/hal.h
@@ -14,7 +14,7 @@ void hal_brownout_output_start();
 void hal_brownout_output_stop();
 
 
-struct PinHandleCreation{
+struct HandleCreation{
   /*no peaking ;)*/
 };
 
@@ -25,7 +25,7 @@ class PinHandle{
  public:
  uint8_t physicalPinNumber();
  PinHandle();
- PinHandle(uint8_t pin, PinHandleCreation* ctx);
+ PinHandle(uint8_t pin, HandleCreation* ctx);
 };
 
 PinHandle create_analog_input(uint8_t pin);
@@ -52,7 +52,27 @@ PinHandle create_inturrupt_pin(uint8_t pin);
 bool inturrupt_pin_read(PinHandle& handle);
 void delete_inturrupt_pin(PinHandle handle);
 
-PinHandle create_digital_output(uint8_t pin);
+PinHandle create_digital_output(uint8_t pin, bool disabled_value = false, bool brownout_val = false,
+                                bool should_disable_output = true, bool should_brown_out = true);
 bool digital_output_read(PinHandle& handle);
 void digital_output_write(PinHandle& handle, bool value);
 void delete_digital_output(PinHandle handle);
+
+
+struct PwmHandle{
+ private:
+  uint8_t _channel;
+ public:
+  uint8_t channel();
+  PwmHandle(uint8_t channel, HandleCreation* ctx);
+};
+
+PwmHandle create_pwm_generator(uint8_t resolution, uint32_t frequecy, uint32_t phase);
+void update_pwm_duty(PwmHandle& handle, uint32_t phase);
+void update_pwm_frequency(PwmHandle& handle, uint32_t frequency);
+void update_pwm_resolution(PwmHandle& handle, uint8_t resolution);
+void delete_pwm_generator(PwmHandle handle);
+
+PinHandle create_pwn_output(uint8_t pin, PwmHandle& pwm_handle, bool disabled_value = false, bool brownout_val = false,
+                                bool should_disable_output = true, bool should_brown_out = true);
+void delete_pwm_output(PinHandle handle);
